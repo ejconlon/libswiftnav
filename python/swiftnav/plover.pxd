26ca02be3fd5c7c5d5109243e9b15a33e91ca339
Scott Kovach <dskovach@gmail.com>
2016-04-04 14:48:19 -0700

    update pyx file, remove plover_main

--- python/swiftnav/plover.pxd
+++ python/swiftnav/plover.pxd
@@ -2,8 +2,8 @@ cimport libc.stdlib
 from libcpp cimport bool
 from prelude cimport (u16, s32)
 
-cdef extern from "libswiftnav/plover/util.h":
-  enum: MAX_STATE_DIM
+cdef extern from "libswiftnav/plover/plover_lib.h":
+  enum: MAX_FILTER_STATE_DIM
 
   ctypedef struct gnss_signal_t:
       u16 sat
@@ -12,19 +12,20 @@ cdef extern from "libswiftnav/plover/util.h":
   ctypedef struct filter_state:
       s32 num_sats
       gnss_signal_t ref
-      gnss_signal_t sats [MAX_STATE_DIM]
+      gnss_signal_t sats [MAX_FILTER_STATE_DIM]
       bool invalid
-      double x [MAX_STATE_DIM]
-      double P [MAX_STATE_DIM * MAX_STATE_DIM]
+      double x [MAX_FILTER_STATE_DIM]
+      double P [MAX_FILTER_STATE_DIM * MAX_FILTER_STATE_DIM]
       double sigma
 
   ctypedef struct measurement:
-      double pseudoranges
-      double carrier_phases
+      double pseudorange
+      double carrier_phase
       double snr
       gnss_signal_t sat_id
 
-cdef extern from "libswiftnav/plover/plover_main.h":
+cdef extern from "libswiftnav/plover/plover_lib.h":
+    filter_state make_filter_state ();
     void kalman_predict_ (const s32 dim, const double * x, const double * P, const double * F, const double * Q, double * x_new, double * P_new);
     void kalman_update_ (const s32 dim, const s32 dim, const double * x, const double * P, const double * y, const double * H, const double * R, double * x_new, double * P_new);
 
@@ -32,3 +33,13 @@ cdef extern from "libswiftnav/plover/plover_main.h":
     void print_all_ (const s32 dim, const double * innovation, const double * S, const double * Si, const double * K, const double * x_new, const double * P_new);
     void observation_model_ (const s32 sats, const double * pseudoranges, const double * carrier_phases, const double * x, const double * base_pos, const double * sat_positions, const double sig_cp, const double sig_pr, double * y, double * H, double * R);
     void sigtest (gnss_signal_t * const x);
+
+    void update_ (const s32 dim, filter_state * const state, const measurement * input_sdiffs, const double * receiver_ecef, const double * sat_positions);
+
+cdef extern from "libswiftnav/signal.h":
+    enum:
+      CODE_INVALID
+      CODE_GPS_L1CA
+      CODE_GPS_L2CM
+      CODE_SBAS_L1CA
+      CODE_COUNT
--- python/swiftnav/plover.pyx
+++ python/swiftnav/plover.pyx
@@ -1,17 +1,73 @@
-cimport plover
+#from swiftnav cimport plover
 import numpy as np
 cimport numpy as np
+import pandas as pd
 from libc.stdlib cimport malloc, free
 
-def test(s):
-    cdef gnss_signal_t x;
-    x.sat = s['sat']
-    x.code = s['code']
-    print x.sat;
-    print x.code;
-    sigtest(&x);
-    print x.sat;
-    print x.code;
+#def to_index(gnss_signal_t s):
+#  dd_N, string-band, string-satid
+
+def convert_code(band):
+    mapping = {
+        '1': CODE_GPS_L1CA,
+        '2': CODE_GPS_L2CM
+    }
+    assert band in mapping
+    return mapping[band]
+
+cdef class CPloverFilter:
+    cdef filter_state state;
+
+    def __init__(self):
+        self.state = make_filter_state();
+
+    def get_P(self):
+        cdef int sats = self.state.num_sats
+        cdef np.ndarray[double, ndim=2, mode="c"] P_flat = np.zeros([sats+2, sats+2])
+        cdef np.ndarray[double, ndim=1, mode="c"] P_max = np.zeros([225])
+        P_max[:] = self.state.P
+
+        P_flat[:] = P_max.reshape((15, 15))[:sats+2, :sats+2]
+        return P_flat
+
+        #self.P = P_flat.reshape((sats+2, sats+2))
+
+    def get_baseline(self):
+        x, y, z = self.state.x[0], self.state.x[1], self.state.x[2]
+        return pd.Series([x, y, z], index=['x', 'y', 'z'])
+
+    def update(self, base_obs, sdiffs, receiver_ecef):
+        # 0 filter_state = self.state
+        # 1 measurements
+        sats = len(sdiffs)
+        #cdef np.ndarray[measurement, ndim=1, mode="c"] measurements = np.empty(sats)
+        cdef measurement *measurements = <measurement *>malloc(sats*sizeof(measurement))
+        #for i, sdiff in enumerate(list(sdiffs.iterrows()))
+        #import ipdb
+        for i in range(sdiffs.shape[0]):
+            sdiff = sdiffs.ix[i]
+
+            #ipdb.set_trace()
+
+            measurements[i].pseudorange = sdiff['pseudorange']
+            measurements[i].carrier_phase = sdiff['carrier_phase']
+            measurements[i].snr = sdiff['signal_noise_ratio']
+            measurements[i].sat_id.sat = int(sdiff['sat'])
+            measurements[i].sat_id.code = convert_code(sdiff['band'])
+
+        # 2 base_pos
+        cdef np.ndarray[double, ndim=1, mode="c"] base_pos = np.zeros([3])
+        base_pos[0] = receiver_ecef[0]
+        base_pos[1] = receiver_ecef[1]
+        base_pos[2] = receiver_ecef[2]
+
+        # 3 sat_pos
+        cdef np.ndarray[double, ndim=2, mode="c"] sat_pos = \
+          np.atleast_2d(base_obs[['sat_x', 'sat_y', 'sat_z']].values.copy(order='c'))
+
+        update_(sats, &self.state, &measurements[0], &base_pos[0], &sat_pos[0,0])
+        free(measurements)
+        #plover.update_()
 
 def kalman_predict(
         np.ndarray[double, ndim=1, mode="c"] x,
@@ -21,7 +77,7 @@ def kalman_predict(
     dim = len(x)
     cdef np.ndarray[double, ndim=1, mode="c"] x_new = np.zeros([dim])
     cdef np.ndarray[double, ndim=2, mode="c"] P_new = np.zeros([dim, dim])
-    plover.kalman_predict_(dim, &x[0], &P[0,0], &F[0,0], &Q[0,0], &x_new[0], &P_new[0,0])
+    kalman_predict_(dim, &x[0], &P[0,0], &F[0,0], &Q[0,0], &x_new[0], &P_new[0,0])
     return (x_new, P_new)
 
 def kalman_update(
@@ -34,13 +90,13 @@ def kalman_update(
     dim = len(y)
     cdef np.ndarray[double, ndim=1, mode="c"] x_new = np.zeros([xdim])
     cdef np.ndarray[double, ndim=2, mode="c"] P_new = np.zeros([xdim, xdim])
-    plover.kalman_update_(xdim, dim, &x[0], &P[0,0], &y[0], &H[0,0], &R[0,0], &x_new[0], &P_new[0,0])
+    kalman_update_(xdim, dim, &x[0], &P[0,0], &y[0], &H[0,0], &R[0,0], &x_new[0], &P_new[0,0])
     return (x_new, P_new)
 
 def inverse(np.ndarray[double, ndim=2, mode="c"] P):
     dim = len(P)
     cdef np.ndarray[double, ndim=2, mode="c"] new = np.zeros([dim, dim])
-    plover.matrix_inverse(dim, &P[0,0], &new[0,0])
+    matrix_inverse(dim, &P[0,0], &new[0,0])
     return new
 
 def observation_model(self,
@@ -63,7 +119,7 @@ def observation_model(self,
     cdef np.ndarray[double, ndim=1, mode="c"] y = np.zeros([2*sats-2])
     cdef np.ndarray[double, ndim=2, mode="c"] H = np.zeros([2*sats-2, sats+2])
     cdef np.ndarray[double, ndim=2, mode="c"] R = np.zeros([2*sats-2, 2*sats-2])
-    plover.observation_model_(sats, &pseudoranges[0], &carrier_phases[0], &x[0], &base_pos[0],
+    observation_model_(sats, &pseudoranges[0], &carrier_phases[0], &x[0], &base_pos[0],
             &sat_pos[0,0], self.sig_cp, self.sig_pr, &y[0], &H[0,0], &R[0,0])
 
     return (y, H, R)
